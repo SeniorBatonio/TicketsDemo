c67c0d457150b26cc7eafa91594b8a0d2d323922
SeniorBatonio <opupep06@gmail.com>
2019-09-22 13:53:08 +0300

    Add RunRepository logic

--- TicketsDemo.Mongo/Repositories/RunRepository.cs
+++ TicketsDemo.Mongo/Repositories/RunRepository.cs
@@ -2,34 +2,49 @@
 using System.Collections.Generic;
 using TicketsDemo.Data.Entities;
 using TicketsDemo.Data.Repositories;
+using System.Linq;
+using MongoDB.Bson;
+using MongoDB.Driver;
 
 namespace TicketsDemo.Mongo.Repositories
 {
     class RunRepository : IRunRepository
     {
+        TicketsContext db = new TicketsContext();
+        ITrainRepository _trainRepo;
+
+        public RunRepository(ITrainRepository trainRepo)
+        {
+            _trainRepo = trainRepo;
+        }
+
+        #region IRunRepository Members
         public void CreateRun(Run run)
         {
-            throw new NotImplementedException();
+            db.Runs.InsertOne(run);
         }
 
         public void DeleteRun(int runId)
         {
-            throw new NotImplementedException();
+            var filter = Builders<Run>.Filter.Eq("_id", runId);
+            db.Runs.FindOneAndDelete(filter);
         }
 
         public PlaceInRun GetPlaceInRun(int placeInRunId)
         {
-            throw new NotImplementedException();
+           return db.PlacesInRun
+                    .Find(x => x.Id == placeInRunId).FirstOrDefault();
         }
 
         public Run GetRunDetails(int runId)
         {
-            throw new NotImplementedException();
+            return db.Runs.Find(r => r.Id == runId).Single();
         }
 
         public List<Run> GetRuns(DateTime start, DateTime end, int? trainId = default(int?))
         {
-            throw new NotImplementedException();
+            return db.Runs.Find(r => r.Date > start && r.Date < end && (trainId == null || r.TrainId == trainId)).ToList();
         }
+        #endregion
     }
 }
