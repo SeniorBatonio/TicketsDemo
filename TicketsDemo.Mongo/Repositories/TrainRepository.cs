f9427dbc480e77799d9caf98a3fed8fb5c477bab
SeniorBatonio <opupep06@gmail.com>
2019-09-22 15:56:56 +0300

    Add TrainRepository logic

--- TicketsDemo.Mongo/Repositories/TrainRepository.cs
+++ TicketsDemo.Mongo/Repositories/TrainRepository.cs
@@ -2,34 +2,38 @@
 using System.Collections.Generic;
 using TicketsDemo.Data.Entities;
 using TicketsDemo.Data.Repositories;
+using System.Linq;
+using MongoDB.Driver;
 
 namespace TicketsDemo.Mongo.Repositories
 {
     class TrainRepository : ITrainRepository
     {
+        TicketsContext db = new TicketsContext();
+
         public void CreateTrain(Train train)
         {
-            throw new NotImplementedException();
+            db.Trains.InsertOne(train);
         }
 
         public void DeleteTrain(Train train)
         {
-            throw new NotImplementedException();
+            db.Trains.FindOneAndDelete(t => t.Id == train.Id);
         }
 
         public List<Train> GetAllTrains()
         {
-            throw new NotImplementedException();
+            return db.Trains.Find(_ => true).ToList();
         }
 
         public Train GetTrainDetails(int trainId)
         {
-            throw new NotImplementedException();
+            return db.Trains.Find(t => t.Id == trainId).FirstOrDefault();
         }
 
         public void UpdateTrain(Train train)
         {
-            throw new NotImplementedException();
+            db.Trains.ReplaceOne(t => t.Id == train.Id, train);
         }
     }
 }
