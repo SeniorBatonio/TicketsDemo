28d55803dbb862fc84f0e9cebe9103be726a81fe
dmitro guz <dmitro.guz@mail.ru>
2019-09-24 22:09:49 +0300

    Delete functionality

--- TicketsDemo.Mongo/TicketsContext.cs
+++ TicketsDemo.Mongo/TicketsContext.cs
@@ -8,16 +8,6 @@ namespace TicketsDemo.Mongo
     {
         IMongoDatabase database; 
 
-        public IMongoCollection<Reservation> Reservations { get; set; }
-
-        public IMongoCollection<PlaceInRun> PlacesInRun { get; set; }
-        public IMongoCollection<Run> Runs { get; set; }
-
-        public IMongoCollection<PriceComponent> PriceComponents { get; set; }
-        public IMongoCollection<Ticket> Tickets { get; set; }
-
-        public IMongoCollection<Carriage> Carriages { get; set; }
-        public IMongoCollection<Place> Places { get; set; }
         public IMongoCollection<Train> Trains { get; set; }
 
         public TicketsContext()
@@ -30,16 +20,6 @@ namespace TicketsDemo.Mongo
             // получаем доступ к самой базе данных
             database = client.GetDatabase(connection.DatabaseName);
 
-            Reservations = database.GetCollection<Reservation>("Reservations");
-
-            PlacesInRun = database.GetCollection<PlaceInRun>("PlacesInRun");
-            Runs = database.GetCollection<Run>("Runs");
-
-            PriceComponents = database.GetCollection<PriceComponent>("PriceComponents");
-            Tickets = database.GetCollection<Ticket>("Tickets");
-
-            Carriages = database.GetCollection<Carriage>("Carriages");
-            Places = database.GetCollection<Place>("Places");
             Trains = database.GetCollection<Train>("Trains");
         }
     }
